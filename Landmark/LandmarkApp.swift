5cd2af1334816c5ec2e5ffebf0e6276908040598
mahao <ma.hao>
2021-11-11 16:15:26 +0800

    Initial Commit

---
+++ Landmark/LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkApp.swift
+//  Landmark
+//
+//  Created by 4wheels on 2021/11/11.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
